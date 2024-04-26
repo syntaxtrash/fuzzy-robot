3ae8d5ce5163357cde6ae6414bd9fe2cf96c740d
Aaron Aco <aaronacodev@gmail.com>
2024-04-26 15:51:31 +0800

    add optional chaining
    Signed-off-by: Aaron Aco <aaronacodev@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -84,3 +84,14 @@ function greet(name: string | null | undefined) {
 }
 
 greet(null);
+
+type Customer = {
+	birthdate?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+	return id === 0 ? null : { birthdate: new Date() };
+}
+
+let customer = getCustomer(0);
+console.log(customer?.birthdate?.getFullYear());
