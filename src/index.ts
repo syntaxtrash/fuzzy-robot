a643bc19b07f1acbad42d2c3ef70acfe46215909
Aaron Aco <aaronacodev@gmail.com>
2024-04-26 15:06:56 +0800

    tuples and enums

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,19 @@ let is_published: boolean = true;
 let level;
 
 let numbers: number[] = [1, 2, 3];
+
+let user: [number, string] = [1, "Juan"];
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+const enum Size {
+	Small = 1,
+	Medium,
+	Large,
+}
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
