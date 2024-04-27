1d47642639f62e75fd46cc2c29f116eeb22b46da
Aaron Aco <aaronacodev@gmail.com>
2024-04-27 14:42:54 +0800

    add static members

--- src/index.ts
+++ src/index.ts
@@ -155,11 +155,36 @@
 // console.log(typeof account);
 // console.log(account instanceof Account);
 
-class SeatAssignment {
-	[seatNumber: string]: string;
+// class SeatAssignment {
+// 	[seatNumber: string]: string;
+// }
+
+// let seats = new SeatAssignment();
+
+// seats.A1 = "Juan";
+// seats.A2 = "Pedro";
+
+class Ride {
+	private static _activeRides: number = 0;
+
+	start() {
+		Ride._activeRides++;
+	}
+
+	stop() {
+		Ride._activeRides--;
+	}
+
+	static get activeRides() {
+		return Ride._activeRides;
+	}
 }
 
-let seats = new SeatAssignment();
+let ride1 = new Ride();
+ride1.start();
 
-seats.A1 = "Juan";
-seats.A2 = "Pedro";
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
+console.log(Ride.activeRides);
