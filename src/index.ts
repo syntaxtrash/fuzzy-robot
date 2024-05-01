2b477c8bb67438e22e422b87cca4217524f8389e
Aaron Aco <aaronacodev@gmail.com>
2024-05-01 12:30:21 +0800

    add peroperty decorators

--- src/index.ts
+++ src/index.ts
@@ -447,27 +447,60 @@
 
 // person.say("Pahingi");
 
-function Capitalize(
-	target: any,
-	methodName: string,
-	descriptor: PropertyDescriptor
-) {
-	const original = descriptor.get;
-	descriptor.get = function () {
-		const result = original?.call(this);
+// function Capitalize(
+// 	target: any,
+// 	methodName: string,
+// 	descriptor: PropertyDescriptor
+// ) {
+// 	const original = descriptor.get;
+// 	descriptor.get = function () {
+// 		const result = original?.call(this);
 
-		return typeof result === "string" ? result.toUpperCase() : result;
+// 		return typeof result === "string" ? result.toUpperCase() : result;
+// 	};
+// }
+// class Person {
+// 	constructor(public firstName: string, public lastName: string) {}
+
+// 	@Capitalize
+// 	get fullname() {
+// 		return `${this.firstName} ${this.lastName}`;
+// 	}
+// }
+
+// let person = new Person("juan", "delacruz");
+
+// console.log(person.fullname);
+
+function MinLength(length: number) {
+	return (target: any, propertyName: string) => {
+		let value: string;
+		const descriptor: PropertyDescriptor = {
+			get() {
+				return value;
+			},
+
+			set(newValue: string) {
+				if (newValue.length < length) {
+					throw new Error(
+						`${propertyName} should atleast ${length} characters long`
+					);
+				} else {
+					value = newValue;
+				}
+			},
+		};
+		Object.defineProperty(target, propertyName, descriptor);
 	};
 }
-class Person {
-	constructor(public firstName: string, public lastName: string) {}
+class User {
+	@MinLength(4)
+	password: string;
 
-	@Capitalize
-	get fullname() {
-		return `${this.firstName} ${this.lastName}`;
+	constructor(password: string) {
+		this.password = password;
 	}
 }
 
-let person = new Person("juan", "delacruz");
-
-console.log(person.fullname);
+let user = new User("1234");
+console.log(user);
