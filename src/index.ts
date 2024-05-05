284df3bf89968c6f014d253edd8c951cf36b4bfc
Aaron Aco <aaronacodev@gmail.com>
2024-05-05 15:09:58 +0800

    add ts as dev depend, add export and import

--- src/index.ts
+++ src/index.ts
@@ -505,21 +505,27 @@
 // let user = new User("1234");
 // console.log(user);
 
-type WatchParameter = {
-	methodName: string;
-	parameterIndex: number;
-};
+// type WatchParameter = {
+// 	methodName: string;
+// 	parameterIndex: number;
+// };
 
-const watchParameters: WatchParameter[] = [];
+// const watchParameters: WatchParameter[] = [];
 
-function Watch(target: any, methodName: string, parameterIndex: number) {
-	watchParameters.push({
-		methodName,
-		parameterIndex,
-	});
-}
-class Vehicle {
-	move(@Watch speed: number) {}
-}
+// function Watch(target: any, methodName: string, parameterIndex: number) {
+// 	watchParameters.push({
+// 		methodName,
+// 		parameterIndex,
+// 	});
+// }
+// class Vehicle {
+// 	move(@Watch speed: number) {}
+// }
 
-console.log(watchParameters);
+// console.log(watchParameters);
+
+import { Circle } from "./shapes";
+let shape = new Circle(10)
+
+console.log(shape);
+
